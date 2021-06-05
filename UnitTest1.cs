70f219b57f3d900728ac0d0f2e4c25028faf2a85
JOTA014 <juanj.delgado@outlook.com>
2021-06-05 00:15:12 -0400

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace JobViteAutomationChallenge
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
